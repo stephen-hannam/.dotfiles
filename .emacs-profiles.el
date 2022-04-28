5320a946c31605f4782b827dc8d4c81f96106077
Stephen Hannam <shannam@cisco.com>
2022-04-29 05:56:48 +1000

    adds files to config and select defaults of emacs distros for chemacs to use:

---
+++ .emacs-profiles.el
@@ -0,0 +1,11 @@
+(("stemacs" . ((user-emacs-directory . "~/.stemacs.d")
+	           (server-name . "stemacs")
+	           (env . (("STEMACSDIR" . "~/.stemacs.d")))
+	       ))
+
+;; emacs distribution: DOOM-emacs
+("doom" . ((user-emacs-directory . "~/doom-emacs")
+	       (server-name . "doom")
+	       (env . (("DOOMDIR" . "~/.doom.d")))
+	     ))
+  )
