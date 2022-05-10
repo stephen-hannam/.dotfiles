a2ea47dd6bd4fa606ac6241932b6487616182332
Stephen Hannam <shannam@cisco.com>
2022-05-11 08:47:38 +1000

    used online example of someone elses early-init file

--- .new-emacs.d/early-init.el
+++ .new-emacs.d/early-init.el
@@ -1,2 +1,17 @@
+(setq gc-cons-threshold most-positive-fixnum)
+
+(setq native-comp-deferred-compilation nil)
+
 ;; disable package.el in favor of straight.el
 (setq package-enable-at-startup nil)
+
+(setq-default inhibit-redisplay t
+              inhibit-message t)
+(add-hook 'window-setup-hook
+          (lambda ()
+            (setq-default inhibit-redisplay nil
+                          inhibit-message nil)
+            (redisplay)))
+
+(set-language-environment "UTF-8")
+(setq default-input-method nil)
