88afe4923478c32c42769b1f0ec5b495e75a627c
Stephen Hannam <shannam@cisco.com>
2022-05-27 06:19:45 +1000

    adds tutorial resource link to completions-init for emvark usage

--- .new-emacs.d/load-inits/completions-init.el
+++ .new-emacs.d/load-inits/completions-init.el
@@ -1,4 +1,7 @@
 ;; vertico, orderless, consult, embark, marginalia, corfu, cape, 'skeletons'?
+;; embark-consult
+;; embark mappings - bookmark, buffer, command, defun, email, expression, face, file, vc, function, general, consult-search, heading, identifier, kill-ring, library, package, ...
+;; https://karthinks.com/software/fifteen-ways-to-use-embark/
 
 (use-package vertico
   :custom
@@ -22,6 +25,15 @@
   (setq completion-styles '(orderless)
         completion-category-defaults nil
         completion-category-overrides '((file (styles . (partial-completion)))))
+  )
+
+(use-package marginalia
+  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+  :init
+  (marginalia-mode)
   )
 
 (provide 'completions-init)
