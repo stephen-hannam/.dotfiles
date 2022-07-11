6adb57be511d5722dcd85c0348f26efdc45508a1
kostoglotov <stephenhannam@gmail.com>
2022-07-11 18:23:00 +1000

    towards getting term-keys to work for keypad keys

--- .exp-emacs.d/modules/try-term-keys.el
+++ .exp-emacs.d/modules/try-term-keys.el
@@ -7,7 +7,7 @@
   (interactive)
   (when term-keys-mode
     (progn
-     (require 'term-keys-kitty)   
+     (require 'term-keys-kitty)
      (with-temp-buffer
        (insert (term-keys/kitty-conf))
        (write-region (point-min) (point-max) "~/.config/kitty/kitty-for-term-keys.conf")
